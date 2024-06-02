c48396b28ed15ab1f4b5b213e72e6a754c2daa77
Sajan Shrestha <s.shrestha@fetchrewards.com>
2024-06-02 06:31:20 -0500

    Add ProjectBanner component

---
+++ src/ProjectBanner.tsx
@@ -0,0 +1,10 @@
+function ProjectBanner() {
+    return (
+        <div>
+            <h1>Progress Pal</h1>
+            <h2>My First React Project</h2>
+        </div>
+    )
+}
+
+export default ProjectBanner;
